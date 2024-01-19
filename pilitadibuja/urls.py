322c3206d5c9a7cec26ac0ecef179986a8385a9f
martinrodriguez24 <martinrodriguezristoff@gmail.com>
2024-01-18 23:15:03 -0300

    feat: add shop urls to main urls

--- pilitadibuja/urls.py
+++ pilitadibuja/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+from apps.shop import urls as shop_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(shop_urls)),
     path('', views.index, name='index'),
-
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
