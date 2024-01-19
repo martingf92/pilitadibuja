2a41a7aae29458bcb2cfa3a918b5495cab63263d
martinrodriguez24 <martinrodriguezristoff@gmail.com>
2024-01-18 23:13:27 -0300

    feat: add shop app in settings

--- pilitadibuja/settings.py
+++ pilitadibuja/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.shop',
 ]
 
 MIDDLEWARE = [
@@ -124,8 +124,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    ('css', BASE_DIR / 'static/css'),
+    ('js', BASE_DIR / 'static/js'),
+    ('media', BASE_DIR / 'static/media'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
